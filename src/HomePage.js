3f74808af06ecfb693d369b9dd7c867186db21f0
DANIEL\Daniel <danielzarodnansky@gmail.com>
2023-11-19 22:06:55 +0100

    refactor

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 
 import NavBar from "./components/NavBar";
-export default function App() {
+export default function HomePage() {
   return <div className="body">
     <NavBar></NavBar>
   </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
             </button>
             <div className="collapse navbar-collapse " id="navbarNav">
                 <input className="form-control me-2 align-content-lg-end w-25" type="search" placeholder="Vyhladat" aria-label="Search"/>
-                <button className="btn btn-outline-light" type="submit">Vyhladat</button>
+                <button className="btn btn-outline-light" type="submit">Vyhľadať</button>
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <a className="nav-link" href="#"> Domov</a>
