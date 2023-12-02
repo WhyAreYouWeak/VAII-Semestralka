6d5375994d2e7c4c77ffc24ffa3c1eeb00fb98d2
DANIEL\Daniel <danielzarodnansky@gmail.com>
2023-12-02 18:02:30 +0100

    home page update

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,6 @@ import NavBar from "./components/NavBar";
 import './style/HomePage.css';
 import ItemTile from "./components/ItemTile";
 import Footer from "./components/Footer";
-import {Link} from "react-router-dom";
 export default function HomePage() {
   return <body>
   <NavBar></NavBar>
