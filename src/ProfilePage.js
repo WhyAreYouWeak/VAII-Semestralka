68ccda4833afa1f5ef81338ebad5facd4dc49bc6
DANIEL\Daniel <danielzarodnansky@gmail.com>
2023-12-04 13:01:58 +0100

    profile page done

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -5,40 +5,60 @@ export default function ProfilePage() {
    return <div>
         <body>
         <NavBar></NavBar>
-        <div className="container mt-5">
+        <div className="container">
             <div className="row">
-                <div className="col-md-4">
-                    <div className="card">
-                        <image src="https://placekitten.com/300/200" className="card-img-top" alt="User"> </image>
-                            <div className="card-body">
-                                <h5 className="card-title">John Doe</h5>
-                                <p className="card-text">Web Developer</p>
-                            </div>
-                    </div>
-                </div>
-
                 <div className="col-md-8">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="card-title">User Information</h5>
+                            <h4 className="card-title">Prihlasovacie informácie</h4>
                             <form>
                                 <div className="form-group">
-                                    <label htmlFor="fullName">Full Name</label>
-                                    <input type="text" className="form-control" id="fullName" value="John Doe"/>
+                                    <label htmlFor="loginEmail">Prihlasovací e-mail</label>
+                                    <input type="text" className="form-control" id="loginEmail" value="zarodnansky@stud.uniza.sk"/>
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" className="form-control" id="email" value="john.doe@example.com"/>
+                                    <label htmlFor="loginPassword">Staré heslo</label>
+                                    <input type="password" className="form-control" id="loginPassword" value=""/>
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="phone">Phone</label>
-                                    <input type="tel" className="form-control" id="phone" value="(555) 123-4567"/>
+                                    <label htmlFor="newPassword">Nové heslo</label>
+                                    <input type="password" className="form-control" id="newPassword" value=""/>
                                 </div>
-                                <div class="form-group">
-                                    <label for="address">Address</label>
-                                    <textarea class="form-control" id="address" rows="3">123 Main Street, Cityville</textarea>
+
+                                <div className="form-group">
+                                    <label htmlFor="newPassword">Nové heslo</label>
+                                    <input type="password" className="form-control" id="newPassword" value=""/>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Save Changes</button>
+                                <button type="submit" className="loginChangeButton btn btn-primary">Zmeniť údaje</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title"> Fakturačné údaje</h4>
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="userName">Meno priezvisko</label>
+                                    <input type="text" className="form-control" id="userName" value="Jožko Mrkvička"/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="phoneNumber">Tel. čislo (+421)</label>
+                                    <input type="number" className="form-control" id="phoneNumber" value=""/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="street">Ulica</label>
+                                    <input type="text" className="form-control" id="street" value=""/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="PSC">PSČ</label>
+                                    <input type="text" className="form-control" id="PSC" value=""/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="city">Mesto</label>
+                                    <input type="text" className="form-control" id="city" value=""/>
+                                </div>
+                                <button type="submit" className="userInformationButton btn btn-primary">Zmeniť údaje</button>
                             </form>
                         </div>
                     </div>
